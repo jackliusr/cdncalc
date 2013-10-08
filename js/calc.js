a726a36db1d1ba95af8e2cd1db05ca6703680247
Philipp Tkachev <zoonman@gmail.com>
2013-10-08 00:44:58 -0400

    Added amazon

--- js/calc.js
+++ js/calc.js
@@ -66,6 +66,7 @@ var
 				{name:'Platinum', price: 409, included: 2048, excessPrice: 0.20 } ];
 			max_and_cachefly(trange, "CacheFly");
 		},
+		
 		MaxCDN: function () {
 			var trange = [ 
 				{name:'#!/bin/start', price:   9, included:   100, excessPrice: 0.08 },
@@ -74,6 +75,7 @@ var
 				{name:'./premium',    price: 499, included: 10000, excessPrice: 0.05 } ];
 			max_and_cachefly(trange, "MaxCDN");
 		},
+		
 		MtProCDN: function () {
 			var traf = $("#traffic_volume").val();
 			var result = 0;
@@ -84,7 +86,6 @@ var
 			else if(traf <= 10240) {
 				result = 20 + (traf-200)*0.15;
 				show_cdn_plan_notes("ProCDN","Default Plan",Math.round((traf-200)*0.15));
-
 			}
 			else {
 				result = 20 + (traf-200)*0.10;
@@ -93,12 +94,14 @@ var
 			result = Math.round(result);
 			$("#traffic_info tr:contains(ProCDN) td:last").html('$' + result);
 		},
+		
 		KeyCDN: function () {
 			var traf = $("#traffic_volume").val();
 			var result = Math.ceil(traf * 0.04);
 			$("#traffic_info tr:contains(KeyCDN) td:last").html('$' + result);
 			show_cdn_plan_notes("KeyCDN","Default Plan");
 		},
+		
 		CDNify: function () {
 			var traf = $("#traffic_volume").val();
 			var result = Math.ceil(traf * 0.06);
@@ -113,6 +116,19 @@ var
 			});
 			$("#traffic_info tr:contains(CDNify) td:last").html('$' + result);
 		},
+		
+		Akamai: function () {
+			var traf = $("#traffic_volume").val();
+			var result = Math.ceil(traf * 0.15);
+			$.each([ [1000, 100], [2500, 250], [5000, 500], [2000, 95], [10000, 900], [100000, 8000] ], function(index) {
+				if (traf <=  this[0]) {
+					result = this[1];
+					return false;
+				}
+			});
+			$("#traffic_info tr:contains(Akamai) td:last").html('$' + result);
+		},
+		
 		SoftlayerCDN: function () {
 			var traf = $("#traffic_volume").val();
 			var result = Math.ceil(  ($("#traffProtocolHTTP:checked").val() ? (traf * 0.12) : 0) +  ($("#traffProtocolHTTPS:checked").val() ? (traf * 0.15) : 0) );
@@ -126,7 +142,6 @@ var
 			var total = 0;
 			total = calculate_total();
 			var continents = [ {name:'US',price:0.3},{name:'SA',price:0.3},{name:'EU',price:0.3},{name:'AU',price:0.8},{name:'AS',price:0.8} ];
-			
 			var note = '', us_cost=0, as_cost=0;
 			$.each(continents, function () {
 				result += traf * ($('#traff'+this.name).val()) * this.price / total;
@@ -143,6 +158,7 @@ var
 			$("#traffic_info tr:contains(GoGrid) td:last").html('$' + result);
 			show_cdn_plan_notes("GoGrid",note);
 		},
+		
 		CDN77: function () {
 			var traf = $("#traffic_volume").val();
 			var result = 0; 
@@ -155,7 +171,6 @@ var
 				{traffic: 1000000, prices: [{continents: ['US','EU'], price: 0.025}, {continents: ['AS','AF'], price: 0.085}, {continents: ['SA'], price: 0.110}]},
 				{traffic:      -1, prices: [{continents: ['US','EU'], price: 0.019}, {continents: ['AS','AF'], price: 0.070}, {continents: ['SA'], price: 0.095}]},
 			];
-			
 			$.each(matrix, function (index) {
 				if (traf <= this.traffic || index == matrix.length-1) {
 					$.each(this.prices, function (){
@@ -173,12 +188,49 @@ var
 					});
 					return false;
 				}
-				
 			});
 			show_cdn_plan_notes("CDN77",note);
 			result = Math.ceil(result);
 			$("#traffic_info tr:contains(CDN77) td:last").html('$' + result);
 		},
+		
+		CloudFront: function () {
+			var traf = $("#traffic_volume").val();
+			var result = 0; 
+			var  note = '', total = 0;
+			total = calculate_total();
+			var matrix = [
+				{traffic:   10000, prices: [{continents:['US','EU'],price: 0.12},{continents:['AS','AU'],price: 0.19},{continents:['SA'],price:0.25},{continents:['JP'],price:0.201},{continents:['IN'],price:0.17}]},
+				{traffic:   40000, prices: [{continents:['US','EU'],price: 0.08},{continents:['AS','AU'],price: 0.14},{continents:['SA'],price:0.20},{continents:['JP'],price:0.148},{continents:['IN'],price:0.13}]},
+				{traffic:  100000, prices: [{continents:['US','EU'],price: 0.06},{continents:['AS','AU'],price: 0.12},{continents:['SA'],price:0.18},{continents:['JP'],price:0.127},{continents:['IN'],price:0.11}]},
+				{traffic:  350000, prices: [{continents:['US','EU'],price: 0.04},{continents:['AS','AU'],price: 0.10},{continents:['SA'],price:0.16},{continents:['JP'],price:0.106},{continents:['IN'],price:0.1}]},
+				{traffic:  524000, prices: [{continents:['US','EU'],price: 0.03},{continents:['AS'],price: 0.08},{continents:['SA'],price:0.140},{continents:['AU'],price:0.095},{continents:['JP'],price:0.085},{continents:['IN'],price:0.1}]},
+				{traffic: 4000000, prices: [{continents:['US','EU'],price:0.025},{continents:['AS'],price: 0.07},{continents:['SA'],price:0.130},{continents:['AU'],price:0.090},{continents:['JP'],price:0.075},{continents:['IN'],price:0.1}]},
+				{traffic:      -1, prices: [{continents:['US','EU'],price: 0.02},{continents:['AS'],price: 0.06},{continents:['SA'],price:0.125},{continents:['AU'],price:0.085},{continents:['JP'],price:0.065},{continents:['IN'],price:0.1}]},
+			];               
+			$.each(matrix, function (index) {
+				if (traf <= this.traffic || index == matrix.length-1) {
+					$.each(this.prices, function (){
+						var cprice = this.price;
+						var group_money = 0;
+						var group_note = '';
+						var this_continents = this.continents;
+						$.each(this.continents, function (ci){
+							result += traf * ($('#traff'+this).length > 0 ? $('#traff'+this).val() : 0) * cprice / total;
+							group_money += traf * ($('#traff'+this).length > 0 ? $('#traff'+this).val() : 0) * cprice / total;
+							group_note += continents_codes[this] + ((ci < this_continents.length - 1) ? ', ':'');  
+						});
+						if(group_money > 0)
+						note += group_note + ' - $' + Math.ceil(group_money) + '<br>';
+					});
+					return false;
+				}
+			});
+			show_cdn_plan_notes("CloudFront",note);
+			result = Math.ceil(result);
+			$("#traffic_info tr:contains(CloudFront) td:last").html('$' + result);
+		},
+		
 		Fastly: function () {
 			var traf = $("#traffic_volume").val();
 			var reqs = $("#inputHttpRequest").val();
@@ -264,4 +316,5 @@ $(document).ready( function(){
 		recalculate();
 	});
 		recalculate();
+	
 });
